aaa18efac84d9201f5a138f9e72fdcd8ac70bdbd
mdpoulter <github@matthewpoulter.co.za>
2019-09-07 22:07:00 +0200

    Initial commit

---
+++ src/AdvancedNumber.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Copyright (c) 2019 Matthew Poulter. All rights reserved.
+ */
+
+namespace SimpleSquid\Nova\Fields\AdvancedNumber;
+
+use Laravel\Nova\Fields\Number;
+
+class AdvancedNumber extends Number
+{
+    /**
+     * The prefix to be used when displaying the number.
+     *
+     * @var string
+     */
+    private $prefix = '';
+
+    /**
+     * The number of decimals to be displayed.
+     *
+     * @var integer
+     */
+    private $decimals = 2;
+
+    /**
+     * The decimal point to be used when displaying the number.
+     *
+     * @var string
+     */
+    private $dec_point = '.';
+
+    /**
+     * The thousands separator to be used when displaying the number.
+     *
+     * @var string
+     */
+    private $thousands_sep = ' ';
+
+    /**
+     * The suffix to be used when displaying the number.
+     *
+     * @var string
+     */
+    private $suffix = '';
+
+    /**
+     * Create a new field.
+     *
+     * @param  string  $name
+     * @param  string|null  $attribute
+     * @param  mixed|null  $resolveCallback
+     * @return void
+     */
+    public function __construct($name, $attribute = null, $resolveCallback = null)
+    {
+        parent::__construct($name, $attribute, $resolveCallback);
+
+        $this->step((string) (0.1 ** $this->decimals))
+             ->withMeta(['textAlign' => 'right'])
+             ->displayUsing(function ($value) {
+                 return !is_null($value) ? $this->prefix . number_format($value, $this->decimals, $this->dec_point, $this->thousands_sep) . $this->suffix : null;
+             });
+
+        $this->textAlign = 'right';
+    }
+
+    /**
+     * Set text alignment
+     *
+     * @param  string  $textAlign
+     * @return $this
+     */
+
+    public function textAlign($textAlign)
+    {
+        $this->textAlign = $textAlign;
+        return $this->withMeta(['textAlign' => $textAlign]);
+    }
+
+    /**
+     * Set the decimal point to be used
+     *
+     * @param  string  $dec_point
+     * @return $this
+     */
+    public function decimalPoint($dec_point)
+    {
+        $this->dec_point = $dec_point;
+        return $this;
+    }
+
+    /**
+     * Set the decimals to be used
+     *
+     * @param  integer  $decimals
+     * @return $this
+     */
+    public function decimals($decimals)
+    {
+        $this->decimals = $decimals;
+        $this->step((string) (0.1 ** $this->decimals));
+        return $this;
+    }
+
+    /**
+     * Set the number of prefix to be used
+     *
+     * @param  string  $prefix
+     * @return $this
+     */
+    public function prefix($prefix)
+    {
+        $this->prefix = $prefix;
+        return $this;
+    }
+
+    /**
+     * Set the suffix to be used
+     *
+     * @param  string  $suffix
+     * @return $this
+     */
+    public function suffix($suffix)
+    {
+        $this->suffix = $suffix;
+        return $this;
+    }
+
+    /**
+     * Set the thousands separator to be used
+     *
+     * @param  string  $thousands_sep
+     * @return $this
+     */
+    public function thousandsSeparator($thousands_sep)
+    {
+        $this->thousands_sep = $thousands_sep;
+        return $this;
+    }
+}
