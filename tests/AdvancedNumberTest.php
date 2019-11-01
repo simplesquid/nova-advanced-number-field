a82513e9cbfa13cb1dce49189104838ad5a8baf6
mdpoulter <github@matthewpoulter.co.za>
2019-11-01 10:43:05 +0200

    Add tests.

---
+++ tests/AdvancedNumberTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace SimpleSquid\Skeleton\Tests;
+
+use PHPUnit\Framework\TestCase;
+use SimpleSquid\Nova\Fields\AdvancedNumber\AdvancedNumber;
+
+class AdvancedNumberTest extends TestCase
+{
+    /** @var \SimpleSquid\Nova\Fields\AdvancedNumber\AdvancedNumber */
+    private $field;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->field = AdvancedNumber::make('Number Field');
+    }
+
+    /** @test */
+    public function field_defaults_to_right_alignment()
+    {
+        $this->assertEquals('right', $this->field->textAlign);
+    }
+
+    /** @test */
+    public function field_defaults_to_step_of_two_decimal_places()
+    {
+        $this->assertArrayHasKey('step', $this->field->meta);
+
+        $this->assertEquals('0.01', $this->field->meta['step']);
+    }
+
+    /** @test */
+    public function field_displays_number_correctly_using_defaults()
+    {
+        $this->field->resolveForDisplay(['number_field' => 12345.678]);
+
+        $this->assertEquals('12 345.68', $this->field->value);
+    }
+
+    /** @test */
+    public function prefix_can_be_set_and_is_displayed()
+    {
+        $this->field->prefix('$');
+
+        $this->field->resolveForDisplay(['number_field' => 12345.678]);
+
+        $this->assertEquals('$12 345.68', $this->field->value);
+    }
+
+    /** @test */
+    public function suffix_can_be_set_and_is_displayed()
+    {
+        $this->field->suffix('%');
+
+        $this->field->resolveForDisplay(['number_field' => 12345.678]);
+
+        $this->assertEquals('12 345.68%', $this->field->value);
+    }
+
+    /** @test */
+    public function number_of_decimal_places_can_be_set_and_is_displayed()
+    {
+        $this->field->decimals(3);
+
+        $this->field->resolveForDisplay(['number_field' => 12345.678]);
+
+        $this->assertEquals('12 345.678', $this->field->value);
+    }
+
+    /** @test */
+    public function decimal_point_can_be_set_and_is_displayed()
+    {
+        $this->field->decimalPoint(',');
+
+        $this->field->resolveForDisplay(['number_field' => 12345.678]);
+
+        $this->assertEquals('12 345,68', $this->field->value);
+    }
+
+    /** @test */
+    public function thousands_separator_can_be_set_and_is_displayed()
+    {
+        $this->field->thousandsSeparator(',');
+
+        $this->field->resolveForDisplay(['number_field' => 12345.678]);
+
+        $this->assertEquals('12,345.68', $this->field->value);
+    }
+
+    /** @test */
+    public function field_resolves_to_actual_value_for_edit_view()
+    {
+        $this->field->resolve(['number_field' => 12345.678]);
+
+        $this->assertEquals(12345.678, $this->field->value);
+    }
+
+    /** @test */
+    public function modifiers_can_be_used_together()
+    {
+        $this->field->prefix('$');
+        $this->field->suffix('c');
+        $this->field->decimals(2);
+        $this->field->decimalPoint(' and ');
+        $this->field->thousandsSeparator(',');
+
+        $this->field->resolveForDisplay(['number_field' => 12345.678]);
+
+        $this->assertEquals('$12,345 and 68c', $this->field->value);
+
+        $this->field->resolve(['number_field' => 12345.678]);
+
+        $this->assertEquals(12345.678, $this->field->value);
+    }
+}
